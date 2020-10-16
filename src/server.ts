a341c6b1c4b791703b1d5deff5e1e6cd207aad8a
Myke Albuquerque Pinto de Oliveira <myke.oliveira@monetizze.com>
2020-10-16 17:43:24 -0300

    hello world

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/users/:id', (request, response) => {
-  console.log(request.query);
-  console.log(request.params);
-  console.log(request.body);
-  return response.json({
-    message: 'Hello World!',
-  });
+  return response.json({ message: 'Hello World!' });
 });
 
 app.listen(3333);
