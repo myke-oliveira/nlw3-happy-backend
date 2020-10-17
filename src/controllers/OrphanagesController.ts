7d605fd73a1b38301d00a4d4bf089a93bdc05763
Myke Albuquerque Pinto de Oliveira <myke.oliveira@monetizze.com>
2020-10-17 12:33:03 -0300

    Validation

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { getRepository } from 'typeorm';
 import orphanageView from '../views/orphanages_view';
 import Orphanage from '../models/Orphanage';
+import * as Yup from 'yup';
 
 export default {
   async index(request: Request, response: Response) {
@@ -48,7 +49,7 @@ export default {
     });
     console.log(images);
 
-    const orphanage = orphanageRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -57,7 +58,26 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required(),
+      }))
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanageRepository.create(data);
 
     await orphanageRepository.save(orphanage);
 
